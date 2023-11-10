7e863d3dba1fed43efba61fbdf45f857436dacd5
Tristan Denyer <tristan@tristandenyer.com>
2023-11-09 21:39:15 -0800

    updated metadata for og:image and description

--- components/layout.js
+++ components/layout.js
@@ -14,13 +14,11 @@ export default function Layout({ children, home }) {
         <link rel="icon" href="/favicon.ico" />
         <meta
           name="description"
-          content="Learn how to build a personal website using Next.js"
+          content="I use decades of technical experience and a uniquely creative approach to envision, design and deliver scalable solutions. Working closely with my leadership and cross-functional teams, I'm able to elegantly solve our most complex challenges."
         />
         <meta
           property="og:image"
-          content={`https://og-image.vercel.app/${encodeURI(
-            siteTitle,
-          )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
+          content={`/images/tristan-denyer-unfurl.jpg`}
         />
         <meta name="og:title" content={siteTitle} />
         <meta name="twitter:card" content="summary_large_image" />
