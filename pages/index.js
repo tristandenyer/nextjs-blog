073e2c976e1c910694156030d44b6f607ec1ebe4
Tristan Denyer <tristan@tristandenyer.com>
2023-11-10 19:57:55 -0800

    added animated gradient to homepage; updated colors

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head';
 import Layout, { siteTitle } from '../components/layout';
+import styles from '../components/layout.module.css';
 import utilStyles from '../styles/utils.module.css';
 import { getSortedPostsData } from '../lib/work.js';
 import Link from 'next/link';
@@ -17,30 +18,32 @@ export async function getStaticProps() {
 
 export default function Home ({ allPostsData }) {
   return (
-    <Layout home>
-      <Head>
-        <title>{siteTitle}</title>
-      </Head>
-      <section className={utilStyles.headingMd}>
-        <p>I use decades of technical experience and a uniquely creative approach to envision, design and deliver scalable solutions. Working closely with my leadership and cross-functional teams, I'm able to elegantly solve our most complex challenges.</p>
-        <p>I also take great pride in my management style. Using progressive hiring and training methods, I devised a systematic approach to inspiring, elevating and providing accountability. As a result of these methods, my team has consistently exceeded our goals while also maintaining the lowest turnover in the engineering department.</p>
-      </section>
-      <hr></hr>
-      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-        <h2 className={utilStyles.headingLg}>Blog</h2>
-        <ul className={utilStyles.list}>
-          {allPostsData.map(({ id, date, title }) => (
-            <li className={utilStyles.listItem} key={id}>
-              <Link href={`/work/${id}`}>{title}</Link>
-              <br />
-              <small className={utilStyles.lightText}>
-                {date && <Date dateString={date} />}
-              </small>
-            </li>
-          ))}
-        </ul>
-      </section>
-      <Articles />
-    </Layout>
+    <div className={styles.gradientBackground}>
+      <Layout home>
+        <Head>
+          <title>{siteTitle}</title>
+        </Head>
+        <section className={utilStyles.headingMd}>
+          <p>I use decades of technical experience and a uniquely creative approach to envision, design and deliver scalable solutions. Working closely with my leadership and cross-functional teams, I'm able to elegantly solve our most complex challenges.</p>
+          <p>I also take great pride in my management style. Using progressive hiring and training methods, I devised a systematic approach to inspiring, elevating and providing accountability. As a result of these methods, my team has consistently exceeded our goals while also maintaining the lowest turnover in the engineering department.</p>
+        </section>
+        <hr></hr>
+        <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+          <h2 className={utilStyles.headingLg}>Blog</h2>
+          <ul className={utilStyles.list}>
+            {allPostsData.map(({ id, date, title }) => (
+              <li className={utilStyles.listItem} key={id}>
+                <Link href={`/work/${id}`}>{title}</Link>
+                <br />
+                <small className={utilStyles.lightText}>
+                  {date && <Date dateString={date} />}
+                </small>
+              </li>
+            ))}
+          </ul>
+        </section>
+        <Articles />
+      </Layout>
+    </div>
   );
 }
