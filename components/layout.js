fc7caaff71bcfe123c5490cbc958776687852d54
Tristan Denyer <tristan@tristandenyer.com>
2024-03-24 18:55:48 -0700

    added favicons

--- components/layout.js
+++ components/layout.js
@@ -12,7 +12,31 @@ export default function Layout({ children, home }) {
   return (
     <div className={styles.container}>
       <Head>
-        <link rel="icon" href="/favicon.ico" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/favicons/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicons/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicons/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/favicons/site.webmanifest" />
+        <link
+          rel="mask-icon"
+          href="/favicons/safari-pinned-tab.svg"
+          color="#6ea2b8"
+        />
+        <meta name="msapplication-TileColor" content="#6ea2b8" />
+        <meta name="theme-color" content="#ffffff" />
         <meta
           name="description"
           content="I use decades of technical experience and a uniquely creative
